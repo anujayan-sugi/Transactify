a0aa5ebf407ea0941cd6a2c53ac0e2b912e486cc
anujayan-sugi <87138924+anujayan-sugi@users.noreply.github.com>
2024-07-20 21:50:29 +0100

    Add files via upload

---
+++ Transactify.py
@@ -0,0 +1,60 @@
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     output = ""
+#     if request.method == 'POST':
+#         destination = request.form.get('destination')
+#         amount = request.form.get('amount')
+#         try:
+#             amount = int(amount)
+#             output = f"Destination: {destination}\n Amount: {amount}"
+#         except ValueError:
+#             output = "Invalid input: Amount must be an integer"
+#     return render_template('index.html', output=output)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+'''
+Short Description:
+Enables simplified transactions by 
+'''
+
+from flask import Flask, render_template, request
+from substrateinterface import SubstrateInterface, Keypair
+keypair = Keypair.create_from_uri('//Alice')
+app = Flask(__name__)
+substrate = SubstrateInterface(
+    url="ws://127.0.0.1:9944",
+    ss58_format=0,
+    type_registry_preset='polkadot'
+)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    output = ""
+    if request.method == 'POST':
+        destination = request.form.get('destination')
+        amount = request.form.get('amount')
+        try:
+            amount = int(amount)
+
+            call = substrate.compose_call(
+                call_module='Balances',
+                call_function='transfer',
+                call_params={
+                    'dest': destination,
+                    'value': amount * 10**15
+                }
+            )
+            payment_info = substrate.get_payment_info(call=call, keypair=keypair)
+
+            output = f"Destination: {destination}, Amount: {amount}\n, Fee: {payment_info['partialFee']}"
+        except ValueError:
+            output = "Invalid input: Amount must be an integer"
+    return render_template('index.html', output=output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
