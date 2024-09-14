be896af62a7de2d6dc660c07cdf3b364f12389d2
anujayan-sugi <87138924+anujayan-sugi@users.noreply.github.com>
2024-09-14 12:43:52 +0100

    Update Transactify.py

--- Transactify.py
+++ Transactify.py
@@ -1,7 +1,7 @@
 # from flask import Flask, render_template, request
 # app1 = Flask(__name__)
 # check above later
-# app
+# app28
 # app = Flask(__name__)
 
 # @app.route('/', methods=['GET', 'POST'])
